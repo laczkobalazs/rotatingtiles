f25459652e423726bb16b290fa467214e30b55f9
Balázs Laczkó <laczko.balazs89@gmail.com>
2020-01-07 13:38:03 +0100

    Adds player movement

--- static/movement.js
+++ static/movement.js
@@ -4,3 +4,27 @@ let tileArray = [];
 for (tile of tiles){
     tileArray.push(tile.dataset.tileNumber)
 }
+
+# let myGame;
+
+function player(position, ) {
+    this.position =
+
+}
+
+function moveUp() {
+    if (player.position > 12) {
+        player.position -= 12;
+    }
+}
+
+function moveDown() {
+    if (player.position < 84) {
+        player.position += 12;
+    }
+}
+function moveLeft() {
+    if (player.position % 12 !== 0) {
+        player.position -= 1;
+    }
+}
