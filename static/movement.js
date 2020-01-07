bb1d96e7a6f103d12d0659c5621f278fc659b1b6
Rhyvan <polyakan@gmail.com>
2020-01-07 13:34:00 +0100

    add player placement functions and set html's square height

--- static/movement.js
+++ static/movement.js
@@ -1,6 +1,27 @@
+// create list of Nodes
 let tiles = document.querySelectorAll('[data-tile-number]');
 
+// create array which holds the number of all tiles. This is the "map".
 let tileArray = [];
 for (tile of tiles){
     tileArray.push(tile.dataset.tileNumber)
 }
+
+// player text
+let playerText = `<i id="player" class="fab fa-accessible-icon"></i>`;
+
+// player placement functions
+function placePlayer(tile){
+    let placement = document.querySelector(`[data-tile-number="${tile}"]`);
+    placement.insertAdjacentHTML("beforeend", playerText);
+}
+
+function removePlayer(){
+    let player = document.querySelector("#player");
+    player.remove();
+}
+
+let replacePlayer = function(tile){
+    removePlayer();
+    placePlayer(tile)
+};
