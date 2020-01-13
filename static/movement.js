f7ce7692f6455cc1b38e5772b8a2f44a9b14b34a
Rhyvan <polyakan@gmail.com>
2020-01-13 18:20:21 +0100

    refactor object behavior, dataholder objects and their usages

--- static/movement.js
+++ static/movement.js
@@ -1,72 +1,30 @@
-/*
-// create list of Nodes
-let tiles = document.querySelectorAll('[data-tile-number]');
-
-// create array which holds the number of all tiles. This is the "map".
-let tileArray = [];
-for (tile of tiles){
-    tileArray.push(tile.dataset.tileNumber)
-}
-
- */
 
 // player text
 let playerText = `<i id="player" class="fab fa-accessible-icon fa-2x" ></i>`;
 
-// create object holder array
-let mapLayoutLevel2 = {
-    "85": "fixRightTile",
-    "86": "fixRightTile",
-    "87": "fixRightTile",
-    "88": "fixUpTile",
-    "65": "fixRightTile",
-    "66": "fixRightTile",
-    "67": "fixRightTile",
-    "68": "fixUpTile",
-    "45": "fixRightTile",
-    "76": "fixUpTile",
-    "52": "fixUpTile",
-    "56": "fixUpTile",
-    "46": "fixUpTile",
-    "34": "fixUpTile",
-    "22": "fixUpTile",
-    "30": "fixLeftTile",
-    "31": "fixLeftTile",
-    "32": "fixLeftTile",
-    "40": "fixLeftTile",
-    "39": "fixLeftTile",
-    "28": "fixDownTile",
-    "26": "rotater",
-    "17": "rotater",
-    "64": "uptile",
-    "44": "uptile",
-    "10": "exit",
+
+let mapLayoutLevelOne = {
+    "player": [49],
+    "exit": [13],
+    "righttile": [16, 19],
+    "lefttile": [28],
+    "rotater": [54, 58],
+    "trigger": [56],
+    "road": [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 25, 26, 27, 28, 29, 30, 32, 33, 34, 37, 38, 39, 41, 42, 44, 45, 46, 49, 50, 51, 53, 54, 56, 57, 58, ]
 };
 
-
-let mapLayout = {
-    "16": "righttile",
-    "28": "lefttile",
-    "19": "righttile",
-    "13": "exit",
-    "54": "rotater",
-    "58": "rotater",
-    "56": "trigger"
+let mapLayoutLevelTwo = {
+    "player": [84],
+    "fixRightTile": [85, 86, 87, 65, 66, 67, 45],
+    "fixUpTile": [88, 68, 76, 56, 46, 34, 22],
+    "fixLeftTile": [30, 31, 32, 40, 39],
+    "fixDownTile": [28],
+    "rotater": [26, 17],
+    "uptile": [64, 44],
+    "exit": [10],
+    "road": [10, 17, 22, 26, 28, 29, 30, 31, 32, 34, 36, 37, 38, 39, 40, 44, 45, 46, 48, 52, 56, 60, 64, 65, 66, 67, 68, 72, 76, 84, 85, 86, 87, 88]
 };
 
-let levelOneLayout = {
-    "16": "lefttile",
-    "28": "lefttile",
-    "19": "righttile",
-    "13": "exit",
-    "54": "rotater",
-    "58": "rotater",
-    "56": "trigger"
-
-};
-let levelOneRoadLayout = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 25, 26, 27, 28, 29, 30, 32, 33, 34, 37, 38, 39, 41, 42, 44, 45, 46, 49, 50, 51, 53, 54, 56, 57, 58, ];
-let levelTwoRoadLayout = [10, 17, 22, 26, 28, 29, 30, 31, 32, 34, 36, 37, 38, 39, 40, 44, 45, 46, 48, 52, 56, 60, 64, 65, 66, 67, 68, 72, 76, 84, 85, 86, 87, 88];
-let roadLayout = [];
 
 let keyCodes = {
     87: "up",
@@ -88,10 +46,10 @@ let mapObjects = {
     "lefttile": {"value": "left", "imgcls": "fa-chevron-left"},
     "downtile": {"value": "down", "imgcls": "fa-chevron-down"},
     "road": {"value": true, "background": "coral"},
-    "fixUpTile": {"value": "fixUp", "imgcls": "fa-long-arrow-alt-up"},
-    "fixRightTile": {"value": "fixRight", "imgcls": "fa-long-arrow-alt-right"},
-    "fixDownTile": {"value": "fixDown", "imgcls": "fa-long-arrow-alt-down"},
-    "fixLeftTile": {"value": "fixLeft", "imgcls": "fa-long-arrow-alt-left"},
+    "fixUpTile": {"value": "up", "imgcls": "fa-long-arrow-alt-up"},
+    "fixRightTile": {"value": "right", "imgcls": "fa-long-arrow-alt-right"},
+    "fixDownTile": {"value": "down", "imgcls": "fa-long-arrow-alt-down"},
+    "fixLeftTile": {"value": "left", "imgcls": "fa-long-arrow-alt-left"},
 };
 
 
@@ -110,7 +68,6 @@ let objBehavior = {
             let triggerObj = document.querySelector(`[data-tile-number='${getTileNum()}'] > i`);
             triggerObj.classList.toggle("fa-check-circle");
             triggerObj.classList.toggle("fa-circle");
-        console.log("door is open now");
     },
     "righttile": function(direction){placePlayerByTile(direction)},
     "lefttile": function(direction){placePlayerByTile(direction)},
@@ -127,7 +84,7 @@ let objBehavior = {
         for(route in newDirection){
             // select the mapObject right/left/up/downtile properties.
             let selectorObj = mapObjects[`${route}tile`];
-            // create a nodelist by the mapObjects property.
+            // create a nodelist from the mapObjects properties.
             let selectedObj = document.querySelectorAll(`.${selectorObj.imgcls}`);
             // create an empty object (tempDic)
             let tempDic = {};
@@ -145,32 +102,23 @@ let objBehavior = {
                 // select the DOM elements and set new data to them.
                 selectorObj = mapObjects[`${rou}tile`];
                 for (let node of objectContainer[rou]){
-                    console.log(node);
+                    //console.log(node);
                     node.classList.remove(`${selectorObj.imgcls}`);
                     node.classList.add(`fa-chevron-${newDirection[rou]}`);
-                    // new mapLayout value must be set, because objectBehavior checks it
-                    mapLayout[node.parentElement.dataset.tileNumber] = `${newDirection[rou]}tile`;
-                    console.log(mapLayout);
-
+                    // new dataset value must be set, because objectBehavior checks it
+                    //mapLayout[node.parentElement.dataset.tileNumber] = `${newDirection[rou]}tile`;
+                    node.dataset.objType = `${newDirection[rou]}tile`;
                 }
             }
         }
-        // below lines are not necessary anymore as there are not only one "forwardtile"
-        //selectorObj.imgcls = `fa-chevron-${newDirection[selectorObj.value]}`;
-        //selectorObj.value = newDirection[selectorObj.value]
     }
 };
-// place objects
-function placeObjects(objClass, tileNum){
+
+
+function placeObjects(objClass, objData, tileNum){
     let placement = document.querySelector(`[data-tile-number="${tileNum}"]`);
-    if (typeof(objClass) == "object"){ // not used, placeRoad is the road placing funtion
-        if (objClass.background) {
-          placement.setAttribute("style", `background:${objClass.background}`)
-        }
-    } else {
-    let tagtxt = `<i class="fas ${objClass}"></i>`;
+    let tagtxt = `<i class="fas ${objClass}" data-obj-type="${objData}"></i>`;
     placement.insertAdjacentHTML("beforeend", tagtxt);
-    }
 }
 
 function placeRoad(array){
@@ -180,22 +128,28 @@ function placeRoad(array){
     }
 }
 let paintMap = function(){
-    if (sessionStorage.getItem("level") === "2"){
-        levelOneRoadLayout = [];
-        Array.prototype.push.apply(levelOneRoadLayout, levelTwoRoadLayout);
-        console.log(levelOneRoadLayout);
-        mapLayout = {};
-        console.log(mapLayout);
-        mapLayout = Object.assign(mapLayout, mapLayoutLevel2);
-        placePlayer(84)
-    } else {
-        placePlayer(49);
+    let myMap;
+    switch(sessionStorage.getItem("level")){
+        case "2":
+            myMap = mapLayoutLevelTwo;
+            break;
+        default:
+            myMap = mapLayoutLevelOne;
     }
-    for (tile in mapLayout) {
-        placeObjects(mapObjects[mapLayout[tile]].imgcls, tile)
+
+    for (let item in myMap) {
+        if (item === "road"){
+            placeRoad(myMap[item])
+        } else if (item === "player") {
+            placePlayer([myMap[item][0]])
+        }
+        else {
+            for (tileNum of myMap[item]){
+                placeObjects(mapObjects[item].imgcls, item, tileNum)
+            }
+        }
     }
-    placeRoad(levelOneRoadLayout);
-    };
+};
 
 
 // player placement functions
@@ -214,42 +168,46 @@ let replacePlayer = function(tile){
     placePlayer(tile)
 };
 
+// get the actual position of player
 function getTileNum(){
     return parseInt(document.querySelector("#player").parentElement.dataset.tileNumber)
 }
 
+// movement event listener
 function onkeyup(event) {
     let toTile = moveDirection(keyCodes[event.keyCode]);
     if (toTile){
         if (allowedToStep(toTile)){
            replacePlayer(toTile);
-           checkObjectBehavior(toTile)
+           checkObjectBehavior()
         }
     }
 }
 
-function checkObjectBehavior(tile){
+function checkObjectBehavior(){
+    let tileNum = getTileNum();
     try{
-        let objName = mapLayout[tile];
+        let objName = document.querySelector(`[data-tile-number="${tileNum}"] > i`).dataset.objType;
         let value = mapObjects[objName].value;
         objBehavior[objName](value);
     } catch {}
 
 }
 
+// movement functions
 function moveDirection(direction){
     let tile = getTileNum();
     switch(direction){
-        case "right" || "fixRight":
+        case "right":
             tile = moveRight(tile);
             break;
-        case "left" || "fixleft":
+        case "left":
             tile = moveLeft(tile);
             break;
-        case "down" || "fixDown":
+        case "down":
             tile = moveDown(tile);
             break;
-        case "up" || "fixUp":
+        case "up":
             tile = moveUp(tile);
     }
     return tile
@@ -307,5 +265,6 @@ function allowedToStep(tile){
     return (placement.style.backgroundColor === mapObjects.road.background)
 }
 
+// These are the running functions ON LOAD ==============================
 document.addEventListener("keyup", onkeyup);
 paintMap();
